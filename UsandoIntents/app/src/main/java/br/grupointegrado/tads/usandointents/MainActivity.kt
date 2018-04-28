c301c610aade23214f87484b69f24ccf5a7cd11a
Paulo Henrique <pauloq_henrique@hotmail.com>
2018-04-27 22:04:47 -0300

    Usando Intents, Botoes: Abrir Site, Abrir Mapa e Compartilhar

--- UsandoIntents/app/src/main/java/br/grupointegrado/tads/usandointents/MainActivity.kt
+++ UsandoIntents/app/src/main/java/br/grupointegrado/tads/usandointents/MainActivity.kt
@@ -1,8 +1,12 @@
 package br.grupointegrado.tads.usandointents
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ShareCompat
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,5 +30,50 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         })
     }
+
+    fun abrirSite(view: View) {
+        val acao = Intent.ACTION_VIEW
+        val dado = Uri.parse("https://www.grupointegrado.br")
+        val intentImplicito = Intent(acao, dado)
+        // verifica se a ação pode ser atendida
+        if (intentImplicito.resolveActivity(packageManager) != null) {
+            startActivity(intentImplicito)
+        }
+    }
+
+    fun abrirMapa(view: View) {
+        val endereco = "Av. Irmãos Pereira, 670 - Centro, Campo Mourão – PR"
+        val builder = Uri.Builder()
+                .scheme("geo")
+                .path("0,0")
+                .appendQueryParameter("q", endereco)
+        // construindo a URI
+        val uriEndereco = builder.build()
+        Log.d("abrirMapa", uriEndereco.toString())
+        val intent = Intent(Intent.ACTION_VIEW, uriEndereco)
+        // verifica se a ação pode ser atendida
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun compartilhar(view: View){
+        val tipoDeMidia = "text/plain"
+        val titulo      = "Compartilhar Mensagem"
+        val texto       = et_mensagem.text.toString()
+        val intentCompartilhar = ShareCompat.IntentBuilder
+                .from(this)
+                .setType(tipoDeMidia)
+                .setChooserTitle(titulo)
+                .setText(texto)
+                .intent
+
+        //verifica se a ação pode ser atendida
+        if (intentCompartilhar.resolveActivity(packageManager) != null ){
+            startActivity(intentCompartilhar)
+        }
+    }
+
+
 
 }
