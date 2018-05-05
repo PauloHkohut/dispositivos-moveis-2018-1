9af33cd3e2e79dd41350ba6e7e6c21a500b5fc50
Paulo Henrique <pauloq_henrique@hotmail.com>
2018-05-04 21:50:40 -0300

    savedStance

--- CicloDeVida/app/src/main/java/br/grupointegrado/tads/ciclodevida/MainActivity.kt
+++ CicloDeVida/app/src/main/java/br/grupointegrado/tads/ciclodevida/MainActivity.kt
@@ -3,43 +3,76 @@ package br.grupointegrado.tads.ciclodevida
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val CONTEUDO_TEXTVIEW: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("Ciclo de vida", "onCreate")
+        if (savedInstanceState != null){
+            if (savedInstanceState.containsKey(CONTEUDO_TEXTVIEW)){
+              val conteudoTextView = savedInstanceState.getString(CONTEUDO_TEXTVIEW)
+                tv_mensagem_log.text = conteudoTextView
+            }
+        }
+
+        //Log.d("Ciclo de vida", "onCreate")
+        imprimir("onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        imprimir("onSaveInstanceState")
+        val conteudoTextView = tv_mensagem_log.text.toString()
+        outState?.putString(CONTEUDO_TEXTVIEW, conteudoTextView)
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("Ciclo de vida", "onStart")
+        //Log.d("Ciclo de vida", "onStart")
+
+        imprimir("onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("Ciclo de vida", "onResume")
+        //Log.d("Ciclo de vida", "onResume")
+
+        imprimir("onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("Ciclo de vida", "onPause")
+        //Log.d("Ciclo de vida", "onPause")
+        imprimir("onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("Ciclo de vida", "onStop")
+        //Log.d("Ciclo de vida", "onStop")
+        imprimir("onStop")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("Ciclo de vida", "onRestart")
+        //Log.d("Ciclo de vida", "onRestart")
+        imprimir("onRestart")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("Ciclo de vida", "onDestroy")
+        //Log.d("Ciclo de vida", "onDestroy")
+        imprimir("onDestroy")
+    }
+
+
+    fun imprimir(mensagem: String){
+        Log.d("Ciclo de vida", mensagem)
+        tv_mensagem_log.append("$mensagem \n")
     }
 }
