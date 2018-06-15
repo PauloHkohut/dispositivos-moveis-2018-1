8dca0669c0c1fa05399bad9b6c55e11445ff2ea1
Paulo Henrique <pauloq_henrique@hotmail.com>
2018-06-15 14:32:01 -0300

    ok

--- ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaContrato.kt
+++ ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaContrato.kt
@@ -4,6 +4,11 @@ import android.provider.BaseColumns
 
 object ListaEsperaContrato {
 
-    
+    internal object Clientes : BaseColumns{
+        const val TABELA = "clientes"
+        const val COLUNA_NOME = "nome"
+        const val COLUNA_TAMANHO_GRUPO = "tamanho_grupo"
+        const val COLUNA_DATA_HORA = "dataHora"
+    }
     
 }
